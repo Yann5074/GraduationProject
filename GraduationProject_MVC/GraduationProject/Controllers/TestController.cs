6a44bd46c283020d97bb56f372aec9749304f46e
Yann <xxx80450@gmail.com>
2025-10-03 23:11:08 +0800

    新增: C# & Vue 檔案基礎設定[All]

---
+++ GraduationProject_MVC/GraduationProject/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using GraduationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.Controllers
+{
+    public class TestController : Controller
+    {
+        dbFurniMartContext _context;
+        public TestController(dbFurniMartContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
