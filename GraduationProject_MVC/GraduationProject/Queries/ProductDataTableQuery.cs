b0ad262ae615da5c1e09cfc6d97bef34a7a7e662
WinnieChangtxt <afannncc.gmail.com>
2025-10-07 01:19:10 +0800

    修改:gitignore以及加入dist資料夾

---
+++ GraduationProject_MVC/GraduationProject/Queries/ProductDataTableQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace GraduationProject.Queries
+{
+    public record ProductDataTableQuery
+    {
+        public string? Search { get; init; }
+        public string SortField { get; init; } = "ProductId";
+        public string SortDir { get; init; } = "ASC"; // or DESC
+        public int Start { get; init; } = 0;
+        public int Length { get; init; } = 10;
+    }
+}
---
+++ GraduationProject_MVC/GraduationProject/Queries/ProductDataTableResult.cs
@@ -0,0 +1,11 @@
+﻿using GraduationProject.DTOs;
+
+namespace GraduationProject.Queries
+{
+    public record ProductDataTableResult
+    {
+        public int TotalRecords { get; init; }
+        public int FilteredRecords { get; init; }
+        public IReadOnlyList<CProductDTO> Items { get; init; } = Array.Empty<CProductDTO>();
+    }
+}
