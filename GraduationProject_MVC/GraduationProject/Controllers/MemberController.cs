29f7360b74bff52dd96b3574e2648fe5f3fa0014
Eddie <a0979551728@gmail.com>
2025-10-07 23:31:44 +0800

    新增:List會員列表與查詢功能初版[CMemberController/List]

--- GraduationProject_MVC/GraduationProject/Controllers/MemberController.cs
+++ GraduationProject_MVC/GraduationProject/Controllers/MemberController.cs
@@ -1,4 +1,7 @@
-﻿using GraduationProject.Models;
+﻿using GraduationProject.Interfaces;
+using GraduationProject.Models;
+using GraduationProject.Services;
+using GraduationProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GraduationProject.Controllers
@@ -6,19 +9,34 @@ namespace GraduationProject.Controllers
     public class MemberController : Controller
     {
         //DI 測試
-        dbFurniMartContext _context;
-        public MemberController(dbFurniMartContext context)
+        private readonly IMemberService _MemberService;
+        public MemberController(IMemberService MemberService)
         {
-            _context = context;
-        }
-        public IActionResult Index()
-        {
-            return View();
+            _MemberService = MemberService;
         }
 
         [HttpGet]
-        public IActionResult List() { 
-            return View();
+        public async Task<IActionResult> List(CMemberListKeyeordViewModel vm) 
+        {
+            // 從 Service 拿資料
+            var allMembers = await _MemberService.MemberListAsync();
+
+            // 有關鍵字就過濾
+            if (!string.IsNullOrWhiteSpace(vm?.Keyword))
+            {
+                string kw = vm.Keyword.Trim();
+                allMembers = allMembers
+                    .Where(p =>
+                        (!string.IsNullOrEmpty(p.Name) && p.Name.Contains(kw, StringComparison.OrdinalIgnoreCase))||
+                        (!string.IsNullOrEmpty(p.Phone) && p.Phone.Contains(kw, StringComparison.OrdinalIgnoreCase))||
+                        (!string.IsNullOrEmpty(p.Address) && p.Address.Contains(kw, StringComparison.OrdinalIgnoreCase)))
+                    .ToList();
+            }
+
+            // 把搜尋關鍵字放進 ViewBag
+            ViewBag.Keyword = vm?.Keyword;
+
+            return View(allMembers);
         }
 
     }
