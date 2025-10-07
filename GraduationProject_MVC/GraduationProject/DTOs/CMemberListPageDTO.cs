469d766196cb71bedf76cb2410a085bf2d7fd73d
Eddie <a0979551728@gmail.com>
2025-10-07 17:32:54 +0800

    新增:List到一半

---
+++ GraduationProject_MVC/GraduationProject/DTOs/CMemberListPageDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace GraduationProject.DTOs
+{
+    public class CMemberListPageDTO
+    {
+        public required IReadOnlyList<CMemberDTO> Items { get; init; }
+        public required int Page { get; init; }
+        public required int PageSize { get; init; }
+        public  int TotalCount { get; init; }
+        public int TotalPages => (int)Math.Ceiling(TotalCount / (double)PageSize);
+    }
+}
--- GraduationProject_MVC/GraduationProject/ViewModels/CMemberListKeyeordViewModel.cs
+++ GraduationProject_MVC/GraduationProject/ViewModels/CMemberListKeyeordViewModel.cs
@@ -8,5 +8,8 @@
             get => _keyword;
             set => _keyword = value?.Trim();
         }
+
+        public int Page { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
     }
 }
