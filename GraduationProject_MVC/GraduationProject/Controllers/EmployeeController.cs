efca65ce566f072e78b95bf4bd73b10e53ae1d82
Erica <erica@gmail.com>
2025-10-07 11:53:01 +0800

    修改: Employee資料表更新

---
+++ GraduationProject_MVC/GraduationProject/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using GraduationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GraduationProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        dbFurniMartContext _context;
+        public EmployeeController(dbFurniMartContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
