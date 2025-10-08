95b8e7555d07611fe004fb35b84ba14fb170bbf0
Erica <erica@gmail.com>
2025-10-08 14:52:33 +0800

    新增: 新增員工功能[Employee]

---
+++ GraduationProject_MVC/GraduationProject/Partials/TEmployee.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraduationProject.Models
+{
+    public partial class TEmployee
+    {
+        [ForeignKey(nameof(FRoleId))]
+        public virtual TEmployeeRole? FRole { get; set; }
+        [ForeignKey(nameof(FStatusId))]
+        public virtual TEmployeeStatus? FStatus { get; set; }
+        [ForeignKey(nameof(FGender))]
+        public virtual TGender? FGenderNavigation { get; set; }
+    }
+}
---
+++ GraduationProject_MVC/GraduationProject/ViewModels/CEmployeeCreateViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace GraduationProject.ViewModels
+{
+    public class CEmployeeCreateViewModel
+    {
+        [Required, StringLength(50)]
+        public string FName { get; set; }
+        [Required, StringLength(10)]
+        public string FPhone { get; set; }
+        [Required, EmailAddress, StringLength(30)]
+        public string FEmail { get; set; }
+        public int FGender { get; set; }
+        public string FBloodType { get; set; }
+        public DateTime? FHireDate { get; set; }
+        public int FRoleId { get; set; }
+        public int FStatusId { get; set; }
+        public string FAccount { get; set; } = default!;
+        public string FPasswords { get; set; } = default!;
+    }
+}
