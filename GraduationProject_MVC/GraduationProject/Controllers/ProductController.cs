d4b9251dec91fb6806ae03cbd3e3364f2c1c1682
Winnie <afannncc@gmail.com>
2025-10-08 16:19:05 +0800

    修改: Lisr[Service]

--- GraduationProject_MVC/GraduationProject/Controllers/ProductController.cs
+++ GraduationProject_MVC/GraduationProject/Controllers/ProductController.cs
@@ -1,9 +1,9 @@
 ﻿using GraduationProject.DTOs;
 using GraduationProject.Interfaces;
-using GraduationProject.ViewModels;
 using GraduationProject.Services;
-
+using GraduationProject.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
 
 namespace GraduationProject.Controllers
 {
@@ -16,32 +16,50 @@ namespace GraduationProject.Controllers
             _ProductService = ProductService;
         }
 
-
         [HttpGet]
-        public async Task<IActionResult> List(CProductSearchKeywordViewModel vm)
+        public IActionResult List(CProductSearchKeywordViewModel vm)
         {
-            // 從 Service 拿資料
-            var allProducts = await _ProductService.ListAsync();
-
-            // 有關鍵字就過濾
-            if (!string.IsNullOrWhiteSpace(vm?.txtKeyword))
-            {
-                string kw = vm.txtKeyword.Trim();
-                allProducts = allProducts
-                    .Where(p =>
-                        (!string.IsNullOrEmpty(p.Name) && p.Name.Contains(kw, StringComparison.OrdinalIgnoreCase)) ||
-                        (!string.IsNullOrEmpty(p.Description) && p.Description.Contains(kw, StringComparison.OrdinalIgnoreCase)))
-                    .ToList();
-            }
-
-            // 把搜尋關鍵字放進 ViewBag
-            ViewBag.Keyword = vm?.txtKeyword;
-
-            // 傳給 Razor View (List.cshtml)
-            return View(allProducts);
+            var query = _ProductService.SearchProduct(vm);
+            return View(query);
         }
 
+        [HttpGet]
+        public IActionResult Create(int? productId)
+        {
+            
 
+            var vm = new CProductCreateViewModel
+            {
+                ProductId = productId.Value
+                // 可能還需要載入商品資訊/會員清單等
+            };
+            return View(vm);
+        }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CProductCreateViewModel vm)
+        {
+            if (!ModelState.IsValid) return View(vm);
+
+            try
+            {
+                var dto = new CProductCreateDTO
+                {
+                    CategoryId = vm.CategoryId,
+                    Name = vm.Name,
+                    
+                };
+
+                int newProductId = _ProductService.CreateProduct(dto); // 回傳訂單ID
+                TempData["createSuccessMessage"] = "訂單建立成功";
+                return RedirectToAction("List", new { id = newProductId });
+            }
+            catch
+            {
+                TempData["createErrorMessage"] = "訂單建立失敗";
+                return View(vm);
+            }
+        }
     }
 }
---
+++ GraduationProject_MVC/GraduationProject/Partials/TProduct.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GraduationProject.Models
+{
+    public partial class TProduct
+    {
+        [ForeignKey(nameof(FProductId))]
+        public TProductVariant ProductVariant { get; set; }
+
+        [ForeignKey(nameof(FProductId))]
+        public TProductAsset ProductAsset { get; set; }
+
+        [ForeignKey(nameof(FCategoryId))]
+        public TCategory Category  { get; set; }
+
+
+    }
+
+
+}
