d157164cd684880c18bdbb4306e94071fdbc3e08
Erica <erica@gmail.com>
2025-10-09 17:12:16 +0800

    新增: edit功能[Employee]

--- GraduationProject_MVC/GraduationProject/DTOs/CEmployeeEditDTO.cs
+++ GraduationProject_MVC/GraduationProject/DTOs/CEmployeeEditDTO.cs
@@ -2,17 +2,17 @@
 {
     public class CEmployeeEditDTO
     {
-        public string FName { get; set; }
-        public string FPhone { get; set; }
-        public string FEmail { get; set; }
-        public string FHeadShot { get; set; }
-        public int FGender { get; set; }
-        public string FBloodType { get; set; }
+        public string? FName { get; set; }
+        public string? FPhone { get; set; }
+        public string? FEmail { get; set; }
+        public string? FHeadShot { get; set; }
+        public int? FGender { get; set; }
+        public string? FBloodType { get; set; }
         public DateTime? FHireDate { get; set; }
-        public int FRoleId { get; set; }
-        public int FStatusId { get; set; }
-        public string FAccount { get; set; } = default!;
-        public string FPasswords { get; set; } = default!;
+        public int? FRoleId { get; set; }
+        public int? FStatusId { get; set; }
+        public string? FAccount { get; set; } = default!;
+        public string? FPasswords { get; set; } = default!;
         public DateTime? FLoginTime { get; set; }
         public DateTime? FChangePasswordTime { get; set; }
     }
--- GraduationProject_MVC/GraduationProject/ViewModels/CEmployeeEditViewModel.cs
+++ GraduationProject_MVC/GraduationProject/ViewModels/CEmployeeEditViewModel.cs
@@ -4,18 +4,24 @@ namespace GraduationProject.ViewModels
 {
     public class CEmployeeEditViewModel
     {
-        public string FName { get; set; }
-        public string FPhone { get; set; }
-        public string FEmail { get; set; }
-        public string FHeadShot { get; set; }
-        public int FGender { get; set; }
-        public string FBloodType { get; set; }
+        public int EmployeeId { get; set; }
+        public string? FName { get; set; }
+        public string? FPhone { get; set; }
+        public string? FEmail { get; set; }
+        public string? FHeadShot { get; set; }
+        public int? FGender { get; set; }
+        public string? FBloodType { get; set; }
         public DateTime? FHireDate { get; set; }
-        public int FRoleId { get; set; }
-        public int FStatusId { get; set; }
-        public string FAccount { get; set; } = default!;
-        public string FPasswords { get; set; } = default!;
+        public int? FRoleId { get; set; }
+        public int? FStatusId { get; set; }
+        public string? FAccount { get; set; } = default!;
+        [StringLength(500)]
+        public string? FPasswords { get; set; } = default!;
+        public string? NewPassword { get; set; }
+        [Compare(nameof(NewPassword), ErrorMessage = "密碼與確認密碼不一致")]
+        public string? ConfirmPassword { get; set; }
         public DateTime? FLoginTime { get; set; }
         public DateTime? FChangePasswordTime { get; set; }
+        public IFormFile? Photo { get; set; }
     }
 }
