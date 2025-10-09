81aa7d77b588accb9ec9722c4990d011bf410e54
yasumasa-svg <yasumasa6104@gmail.com>
2025-10-09 14:47:57 +0800

    新增 Controller 新刪修

---
+++ GraduationProject_MVC/GraduationProject/ViewModels/ChatRoomViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GraduationProject.ViewModels
+{
+    public class ChatRoomViewModel
+    {
+
+        public int FChatRoomId { get; set; }
+
+        public string FStatus { get; set; }
+
+        public string FContent { get; set; }
+
+        public DateTime? FCreatedAt { get; set; }
+
+        public DateTime? FClosedAt { get; set; }
+
+    }
+}
