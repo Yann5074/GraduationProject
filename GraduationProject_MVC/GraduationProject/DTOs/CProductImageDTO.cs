634edde97aa77dd88607ef8fb08f168fb01b5a3d
WinnieChangtxt <afannncc.gmail.com>
2025-10-09 16:15:16 +0800

    新增:Detail詳細、編輯功能[ProductController/Detail/Edit]

---
+++ GraduationProject_MVC/GraduationProject/DTOs/CProductImageDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GraduationProject.DTOs
+{
+    public class CProductImageDTO
+    {
+        public string Url { get; set; }
+        public bool? IsPrimary { get; set; }
+        public int? SortOrder { get; set; }
+    }
+}
--- GraduationProject_MVC/GraduationProject/Interfaces/IProductService.cs
+++ GraduationProject_MVC/GraduationProject/Interfaces/IProductService.cs
@@ -8,11 +8,10 @@ namespace GraduationProject.Interfaces
     {
         IEnumerable<CProductDTO> SearchProduct(CProductSearchKeywordViewModel vm);
 
-        public int CreateProduct(CProductCreateDTO dto);
+        CProductDetailDTO GetProductDetail(int id); // 同步
 
-        
-
-
+        CProductEditViewModel GetProductForEdit(int id); // 讀取資料進編輯頁
+        bool UpdateProduct(CProductEditViewModel vm);    // 寫回資料庫
     }
 
 }
