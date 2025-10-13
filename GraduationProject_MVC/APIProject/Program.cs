0ecb8a85bc13c023c199f23f9268ba390b882d68
Yann <xxx80450@gmail.com>
2025-10-13 10:25:12 +0800

    新增: 完成前台APIController基礎設定 [Program.cs/appsettings.json/Middleware]

--- GraduationProject_MVC/APIProject/Program.cs
+++ GraduationProject_MVC/APIProject/Program.cs
@@ -1,3 +1,9 @@
+using ApiProject.Interfaces;
+using ApiProject.Middleware;
+using ApiProject.Models;
+using ApiProject.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy(name: "VueClient", policy =>
+    {
+        policy.WithOrigins(
+            "http://localhost:5173" //測試環境未來可能補上線環境
+            )
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddDbContext<dbFurniMartContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbFurniMart")));
+
+// 注入 COrderService
+builder.Services.AddScoped<IOrderService, COrderService>();
 
 var app = builder.Build();
 
@@ -16,8 +38,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+//設定例外錯誤的中介層
+app.UseMiddleware<ExceptionMiddleware>();
 
+app.UseHttpsRedirection();
+app.UseCors("VueClient");
 app.UseAuthorization();
 
 app.MapControllers();
---
+++ GraduationProject_MVC/APIProject/Services/COrderService.cs
@@ -0,0 +1,51 @@
+﻿using ApiProject.DTOs;
+using ApiProject.Interfaces;
+using ApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProject.Services
+{
+    public class COrderService:IOrderService
+    {
+        private readonly dbFurniMartContext _context;
+        public COrderService(dbFurniMartContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResOrderDTO>> GetAllOrdersAsync()
+        {
+            var query = _context.TOrders
+                .Include(o => o.Member)
+                .Include(o => o.Employee)
+                .Include(o => o.OrderStatus)
+                .Include(o => o.PaymentStatus)
+                .Include(o => o.DeliveryStatus)
+                .Include(o => o.LogisticsProvider)
+                .Where(o => o.FIsDeleted != 1)
+                .Select(o => new ResOrderDTO
+                {
+                    OrderId = o.FOrderId.ToString(),
+                    MemberName = o.Member.FName,
+                    EmployeeId = o.FEmployeeId,
+                    EmployeeName = o.Employee.FName,
+                    OrderTime = o.FOrderTime.ToString(),
+                    OrderStatusId = o.FOrderStatus,
+                    OrderStatus = o.OrderStatus.FStatusName,
+                    PaymentStatusId = o.FPaymentStatus,
+                    PaymentStatus = o.PaymentStatus.FStatusName,
+                    DeliveryStatusId = o.FDeliveryStatus,
+                    DeliveryStatus = o.DeliveryStatus.FDeliveryStatusName
+                });
+            return await query.ToListAsync();
+        }
+
+        public async Task<List<ResOrderDTO>> GetOrdersByIdAndProdNameAsync(string? keyword)
+        {
+            var query = _context.TOrders
+                .Include(o => o.OrderDetail)
+                .ThenInclude(od => od.FProductVariantId)
+                .ThenInclude(pro => pro.)
+        }
+    }
+}
