e41b36b6c436622cc8c21241bf0b0e451910edb9
Eddie <a0979551728@gmail.com>
2025-10-14 11:01:56 +0800

    修改:資料表更新

--- GraduationProject_MVC/GraduationProject/Models/TorderStatusHistory.cs
+++ GraduationProject_MVC/GraduationProject/Models/TorderStatusHistory.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 namespace GraduationProject.Models;
 
-public partial class TorderStatusHistory
+public partial class TOrderStatusHistory
 {
     public int FHistoryId { get; set; }
 
