4a36ee94a8afa5f5a53e4fc451d9591042a2e46f
Eddie <a0979551728@gmail.com>
2025-10-15 16:23:37 +0800

    新增:填寫會員個資[ApiProject/MemberController/UpdateMe]、登出移除會員資料的Cookie[ApiProject/MemberController/Logout] 修改:註冊調整欄位[ApiProject/MemberController/Create]、登入會員資料存到Cookie[ApiProject/MemberController/Login]

--- GraduationProject_MVC/APIProject/DTOs/ReqMemberCreateAccountDTO.cs
+++ GraduationProject_MVC/APIProject/DTOs/ReqMemberCreateAccountDTO.cs
@@ -1,16 +1,15 @@
 ﻿namespace ApiProject.DTOs
 {
-    public class ReqMemberUpdateDTO
+    public class ReqMemberCreateAccountDTO
     {
-        
         public string Name { get; set; }
-        public string DisplayName { get; set; }
-        public int? Gender { get; set; } 
-        public string Email { get; set; }
         public string Phone { get; set; }
-        public string Address { get; set; }
+        public string Email { get; set; }
+        public string Account { get; set; }
+        public string Password { get; set; }
 
         //public bool? FPhoneState { get; set; }
         //public bool? FEmailState { get; set; }
+
     }
 }
