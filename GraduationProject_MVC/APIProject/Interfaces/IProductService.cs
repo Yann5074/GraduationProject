48958592c80556fdaeb7cdafd59e2a6db5cb63f3
Winnie <afannncc@gmail.com>
2025-10-15 14:47:53 +0800

    新增:搜尋產品 [ProductService/GetProductByProdNameAsync]

--- GraduationProject_MVC/APIProject/Interfaces/IProductService.cs
+++ GraduationProject_MVC/APIProject/Interfaces/IProductService.cs
@@ -4,6 +4,9 @@ namespace ApiProject.Interfaces
 {
     public interface IProductService
     {
-        Task<List<ResProductDTO>> AllProductAsync(CancellationToken ct = default);
+        Task<List<ResProductDTO>> GetAllProductAsync(CancellationToken ct = default);
+
+        Task<List<ResProductDTO>> GetProductByProdNameAsync(string? keyword, CancellationToken ct = default);
     }
+
 }
