7d35161fe3382df02a3c9d0a186411d762f03eaf
Erica <erica@gmail.com>
2025-10-15 10:27:43 +0800

    修改: hero區塊懸浮樣式[style.css]

--- GraduationProjectFrontEnd/src/App.vue
+++ GraduationProjectFrontEnd/src/App.vue
@@ -22,9 +22,9 @@ const isActive = (path) => route.path === path
 				<div class="collapse navbar-collapse" id="navbarsFurni">
 					<ul class="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
 						<li class="nav-item" :class="{ active: isActive('/') }">
-							<RouterLink class="nav-link" to="/">Home</RouterLink>
+							<RouterLink class="nav-link" to="/">首頁</RouterLink>
 						</li>
-						<li class="nav-item" :class="{ active: isActive('/shop') }"><RouterLink class="nav-link" to="/shop">Shop</RouterLink></li>
+						<li class="nav-item" :class="{ active: isActive('/shop') }"><RouterLink class="nav-link" to="/shop">購物</RouterLink></li>
 						<li class="nav-item" :class="{ active: isActive('/about') }"><RouterLink class="nav-link" to="/about">About</RouterLink></li>
 						<li class="nav-item" :class="{ active: isActive('/service') }"><RouterLink class="nav-link" to="/services">Services</RouterLink></li>
 						<li class="nav-item" :class="{ active: isActive('/blog') }"><RouterLink class="nav-link" to="/blog">Blog</RouterLink></li>
