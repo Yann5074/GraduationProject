6a1a330603f01f211cf0fd849877df5f892f5af6
WinnieChangtxt <afannncc.gmail.com>
2025-10-16 16:48:56 +0800

    新增: 篩選跟單一商品詳細內容[ProductService/GetFilterOptionsAsync/GetProductByIdAsync][ProductController/GetProduct/GetFilterOptions]

--- GraduationProject_MVC/APIProject/Interfaces/IProductService.cs
+++ GraduationProject_MVC/APIProject/Interfaces/IProductService.cs
@@ -4,14 +4,13 @@ namespace ApiProject.Interfaces
 {
     public interface IProductService
     {
-        
+
 
         Task<List<ResProductDTO>> GetProductByProdNameAsync(string? keyword, CancellationToken ct = default);
 
-        //Task<bool> SoftDeleteAsync(int id);
-        Task<List<ResProductListDTO>> GetAllProductsAsync(ReqProductFilterDTO filter);
-        //Task<ResFilterOptionsDTO> GetFilterOptionsAsync();
-        //Task<ResProductDetailDTO> GetProductByIdAsync(int id);
+        Task<ResultPagedDTO<ResProductListDTO>> GetAllProductsAsync(ReqProductFilterDTO filter);
+        Task<ResFilterOptionsDTO> GetFilterOptionsAsync();
+        Task<ResProductDetailDTO> GetProductByIdAsync(int id);
         //Task<bool> UpdateStockAsync(int variantId, int quantity);
 
     }
