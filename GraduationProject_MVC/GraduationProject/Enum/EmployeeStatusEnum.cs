30dce5c3734e7e5cdef417dd9b65db2c29966b8d
Erica <erica@gmail.com>
2025-10-19 16:36:21 +0800

    修改: 密碼複雜度修改[CEmployeeService]

--- GraduationProject_MVC/GraduationProject/Enum/EmployeeStatusEnum.cs
+++ GraduationProject_MVC/GraduationProject/Enum/EmployeeStatusEnum.cs
@@ -1,10 +1,12 @@
 ﻿namespace GraduationProject.Enum
 {
-    public enum CEmployeeStatusEnum
+    public enum EmployeeStatusEnum
     {
         Employed = 1,   // 在職
         Resigned = 2,   // 離職
         Retired = 3,   // 退休
         Deleted = 4    // 註銷
     }
+
+    public enum ResetPasswordResult { Ok, InvalidLink, WeakPassword }
 }
