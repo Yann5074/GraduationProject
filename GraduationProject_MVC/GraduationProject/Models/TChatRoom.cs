90c174d447837c79ccf31e201b64f1bea80c8f0c
yasumasa-svg <yasumasa6104@gmail.com>
2025-10-19 16:46:02 +0800

    修改:後台的ChatRoom controller

--- GraduationProject_MVC/GraduationProject/Models/TChatRoom.cs
+++ GraduationProject_MVC/GraduationProject/Models/TChatRoom.cs
@@ -30,9 +30,4 @@ public partial class TChatRoom
     public int? FIsBotActive { get; set; }
 
     public string FBotStateJson { get; set; }
-
-    // 🌼 導覽屬性（多對一）
-    public virtual TMember? FMember { get; set; }  // ✔ 導覽屬性（指向 TMember）
-    // 🌼 導覽屬性（一對多）
-    public virtual ICollection<TMessage> Messages { get; set; } = new List<TMessage>();
 }
