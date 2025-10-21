82df6508b4ef19ca1a6e474c63a62784ca007896
Erica <erica@gmail.com>
2025-10-21 11:14:28 +0800

    新增: 取得登入員工資料[CUserContextService, IUserContextService]

---
+++ GraduationProject_MVC/GraduationProject/Services/CUserContextService.cs
@@ -0,0 +1,30 @@
+﻿using GraduationProject.Dictionary;
+using GraduationProject.DTOs;
+using GraduationProject.Interfaces;
+using System.Text.Json;
+
+namespace GraduationProject.Services
+{
+    public class CUserContextService : IUserContextService
+    {
+        private readonly IHttpContextAccessor _http;
+        public CUserContextService(IHttpContextAccessor http) => _http = http;
+
+        public int GetEmployeeId()
+        {
+            var json = _http.HttpContext?.Session.GetString(CEmployeeDictionary.SK_LOGINED_USER);
+            var user = string.IsNullOrEmpty(json) ? null : JsonSerializer.Deserialize<SessionUser>(json);
+
+            return user.Id;
+        }
+
+        public string GetEmployeeName()
+        {
+            var json = _http.HttpContext?.Session.GetString(CEmployeeDictionary.SK_LOGINED_USER);
+            var user = string.IsNullOrEmpty(json) ? null : JsonSerializer.Deserialize<SessionUser>(json);
+
+            return user?.Name ?? user?.Account;
+        }
+
+    }
+}
