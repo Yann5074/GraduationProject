63c2efb3ffb1accc88daa4ebaaa522b48b26a85f
yasumasa-svg <yasumasa6104@gmail.com>
2025-10-24 15:57:36 +0800

    修改: ChatRoom 登入者驗證功能 [ChatRoomController]

---
+++ GraduationProject_MVC/APIProject/DTOs/ReqSendMessageDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProject.DTOs
+{
+    public class ReqSendMessageDTO
+    {
+       public int chatRoomId {  get; set; }
+       public string content { get; set; }
+       public string? visitorId {  get; set; }
+       public int? employeeId { get; set; }
+    }
+}
