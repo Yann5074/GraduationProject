c91024a6f5a4446cd200abf3c4bda4c3ea41d71f
Yann <xxx80450@gmail.com>
2025-10-27 17:12:28 +0800

    新增: 購物車明細列出功能 [Cart.vue]
    Signed-off-by: Yann <xxx80450@gmail.com>

---
+++ GraduationProjectFrontEnd/src/api/Cart.js
@@ -0,0 +1,38 @@
+import http from './axios'
+import {handleApiResult} from '@/utils/apiHelper'
+import { mapToCartDTOList } from '@/dtos/CartDTO'
+
+
+
+// 列出購物車內容
+export const getAllCarts = async () =>{
+    try{
+        const result = await http.get('/Cart');
+        console.log(result)
+        return mapToCartDTOList(result.data);
+    }catch(err){
+        console.error('取得購物車資料錯誤', err);
+        throw err
+    }
+}
+// 刪除購物車內某商品
+export const deleteItem = async() =>{
+    try{
+        const result = await http.delete(`/Cart/item/${cartItemId}`)
+        console.log(result)
+        console.log(result.data)
+        return handleApiResult(result.data)
+    }catch(err){
+        console.error('移除購物車商品失敗', err)
+        throw err
+    }
+}
+// 刪除購物車
+
+// 編輯購物車物品數量
+
+// 商品加入購物車 (有登入)
+
+// 確認購物車是否正常
+
+// 登入時購物車轉換
