251313423246e7e5139363c80834c492a8b66873
Yann <xxx80450@gmail.com>
2025-10-30 13:44:04 +0800

    新增: ECPay 還沒做完
    Signed-off-by: Yann <xxx80450@gmail.com>

---
+++ GraduationProject_MVC/APIProject/Controllers/ECPayController.cs
@@ -0,0 +1,22 @@
+﻿using ApiProject.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ECPayController : ControllerBase
+    {
+        private const string MerchantID = "3002607";
+        private const string HashKey = "pwFHCqoQZGmho4w6";
+        private const string HashIV = "EkRm7iFT261dpevs";
+
+        // Post: api/ECPay/checkout
+        [HttpPost("checkout")]
+        //public IActionResult Checkout(ReqECPayDTO reqDTO)
+        //{
+        //    retutrn null;
+        //}
+
+    }
+}
