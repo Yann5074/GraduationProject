2738a1a6ddf4ebb3af08db57b8dc698809c0906a
Yann <xxx80450@gmail.com>
2025-10-30 13:44:04 +0800

    新增: ECPay 還沒做完
    Signed-off-by: Yann <xxx80450@gmail.com>

---
+++ GraduationProject_MVC/APIProject/DTOs/ReqECPayDTO.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiProject.DTOs
+{
+    public class ReqECPayDTO
+    {
+        public string MerchantID { get; set; }
+
+        public string MerchantTradeNo { get; set; }
+
+        public string MerchantTradeDate { get; set; }
+
+        public string PaymentType { get; set; }
+
+        public int TotalAmount { get; set; }
+
+        public string TradeDesc { get; set; }
+
+        public string ItemName { get; set; }
+    }
+}
