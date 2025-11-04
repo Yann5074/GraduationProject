aef43f04e424c29ec4b1cc829468a4fc8225670f
yasumasa-svg <yasumasa6104@gmail.com>
2025-11-04 15:32:21 +0800

    新增: 添加 ReqAiQueryDTO、IAiService、SmartAiService 和 ChatAiProductService 類別

---
+++ GraduationProject_MVC/APIProject/DTOs/ReqAiQueryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProject.DTOs
+{
+    public class ReqAiQueryDTO
+    {
+        public string? Category { get; set; }
+        public decimal? PriceMin { get; set; }
+        public decimal? PriceMax { get; set; }
+    }
+}
---
+++ GraduationProject_MVC/APIProject/Infrastructure/IAiService.cs
@@ -0,0 +1,7 @@
+﻿namespace ApiProject.Infrastructure
+{
+    public interface IAiService
+    {
+        Task<string> CompleteAsync(string userText, string? clientSummary = null);
+    }
+}
